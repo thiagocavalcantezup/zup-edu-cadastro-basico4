27854b74a0bfff12e8ddac796d602d3decfd8255
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-03-29 13:00:12 -0300

    Add Autor entity class

---
+++ src/main/java/br/com/zup/handora/cadastrobasico4/models/Autor.java
@@ -0,0 +1,50 @@
+package br.com.zup.handora.cadastrobasico4.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.validator.constraints.br.CPF;
+
+@Entity
+@Table(name = "autores")
+public class Autor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 120)
+    private String nome;
+
+    @Column(nullable = false, length = 120)
+    private String email;
+
+    @Column(nullable = false, length = 2500)
+    private String descricao;
+
+    @Column(nullable = false)
+    @CPF
+    private String cpf;
+
+    /**
+     * @deprecated Construtor de uso exclusivo do Hibernate
+     */
+    @Deprecated
+    public Autor() {}
+
+    public Autor(String nome, String email, String descricao, @CPF String cpf) {
+        this.nome = nome;
+        this.email = email;
+        this.descricao = descricao;
+        this.cpf = cpf;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+}

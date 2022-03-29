03bb4a8cabfeae01b860be64a717e77be01e8350
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-03-29 13:02:29 -0300

    Add Repository dependency to Controller

--- src/main/java/br/com/zup/handora/cadastrobasico4/controllers/AutorController.java
+++ src/main/java/br/com/zup/handora/cadastrobasico4/controllers/AutorController.java
@@ -3,10 +3,18 @@ package br.com.zup.handora.cadastrobasico4.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.zup.handora.cadastrobasico4.repositories.AutorRepository;
+
 @RestController
 @RequestMapping(AutorController.BASE_URI)
 public class AutorController {
 
     public final static String BASE_URI = "/autores";
+
+    private final AutorRepository autorRepository;
+
+    public AutorController(AutorRepository autorRepository) {
+        this.autorRepository = autorRepository;
+    }
 
 }

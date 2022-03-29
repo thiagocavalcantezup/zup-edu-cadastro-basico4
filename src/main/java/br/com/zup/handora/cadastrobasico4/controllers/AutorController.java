c9cd7c046eed2d6df104cf96e1454ea3a437fc53
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-03-29 13:06:43 -0300

    Add Controller method to create a new Autor

--- src/main/java/br/com/zup/handora/cadastrobasico4/controllers/AutorController.java
+++ src/main/java/br/com/zup/handora/cadastrobasico4/controllers/AutorController.java
@@ -1,8 +1,18 @@
 package br.com.zup.handora.cadastrobasico4.controllers;
 
+import java.net.URI;
+
+import javax.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import br.com.zup.handora.cadastrobasico4.models.Autor;
+import br.com.zup.handora.cadastrobasico4.models.AutorDTO;
 import br.com.zup.handora.cadastrobasico4.repositories.AutorRepository;
 
 @RestController
@@ -15,6 +25,18 @@ public class AutorController {
 
     public AutorController(AutorRepository autorRepository) {
         this.autorRepository = autorRepository;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> create(@RequestBody @Valid AutorDTO autorDTO,
+                                       UriComponentsBuilder uriComponentsBuilder) {
+        Autor autor = autorRepository.save(autorDTO.paraAutor());
+
+        URI location = uriComponentsBuilder.path(BASE_URI + "/{id}")
+                                           .buildAndExpand(autor.getId())
+                                           .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 
 }
